321b787e69d57a63f0d2429f8c7dc744da5f00c4
Armin Hajibeygi <armin.beygi2000@gmail.com>
2024-05-09 22:47:43 +0330

    feat: create df from gsheet

--- gsheet.py
+++ gsheet.py
@@ -1,4 +1,5 @@
 import gspread
+import pandas as pd
 from const import gsheet_name
 
 
@@ -10,7 +11,16 @@ class Sheet:
         self.sheet = self.client.open(self.sheet_name).get_worksheet_by_id(
             self.sheet_id
         )
+        self.df = None
+        self.data = None
+        self.create_df()
 
     def get_sheet_id(self):
         sheets = self.client.open(self.sheet_name).worksheets()
         return sheets[-1].id
+
+    def create_df(self):
+        self.data = self.sheet.get_all_values()
+        self.df = pd.DataFrame(self.data[1:], columns=self.data[0]).set_index(
+            self.data[0][0]
+        )
