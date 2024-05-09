a30879b6f52e25fdd29e4a03e5ceb3ab8b01f8c1
Armin Hajibeygi <armin.beygi2000@gmail.com>
2024-05-09 23:15:41 +0330

    style: better code

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from datetime import datetime
 from typing import Final
 from telegram import Update
@@ -10,16 +9,20 @@ from const import TOKEN, ids, gsheet_name
 BOT_TOKEN: Final = TOKEN
 sheet = Sheet(gsheet_name)
 sheet_df = sheet.df
+NO_ACCESS = "You don't have access to this bot."
+
+
+def check_access(update: Update) -> bool:
+    user_id = update.effective_user.id
+    return user_id in ids
 
 
 async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
-
-    if user_id not in ids:
-        text = "You don't have access to this bot."
-    else:
-        text = "Hello, I'm Tik8 bot! Thanks for using me!"
-
+    text = (
+        NO_ACCESS
+        if not check_access(update)
+        else "Hello, I'm Tik8 bot! Thanks for using me!"
+    )
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text,
@@ -30,35 +33,24 @@ async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TY
 async def today_task_command_handler(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ):
-    user_id = update.effective_user.id
-
-    if user_id not in ids:
-        text = "You don't have access to this bot."
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=text,
-            reply_to_message_id=update.effective_message.id,
-        )
-
+    if not check_access(update):
+        text = NO_ACCESS
     else:
-        tasks = []
         today = datetime.today().strftime("%d %b").lstrip("0").replace(" 0", " ")
+        tasks = [
+            f"Lesson <u>{index}</u> for the <u>{column}</u> time"
+            for index, row in sheet.df.iterrows()
+            for column in sheet.df.columns
+            if row[column] == today
+        ]
+        text = "\n".join(tasks) if tasks else "You have no other tasks for today!"
 
-        for index, row in sheet.df.iterrows():
-            for column in sheet.df.columns:
-                if row[column] == today:
-                    tasks.append(f"Lesson <u>{index}</u> for the <u>{column}</u> time")
-
-        if tasks:
-            text = "\n".join(tasks)
-        else:
-            text = "You have no other tasks for today!"
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=text,
-            reply_to_message_id=update.effective_message.id,
-            parse_mode="HTML",
-        )
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_to_message_id=update.effective_message.id,
+        parse_mode="HTML",
+    )
 
 
 if __name__ == "__main__":
