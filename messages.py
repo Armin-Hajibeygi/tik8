e4ac64e0ae279bf11cc925516fdfe8b5bffafe57
Armin Hajibeygi <armin.beygi2000@gmail.com>
2024-06-06 10:09:21 +0330

    feat: update NO_ACCESS message

--- messages.py
+++ messages.py
@@ -1,5 +1,5 @@
 START_MESSAGE = "Hello, I'm Tik8 bot! Thanks for using me!"
-NO_ACCESS = "You don't have access to this bot."
+NO_ACCESS = "You don't have access to this bot. \nFirst you should use \\signup"
 NOT_VALID_WORKSHEET_NAME = "Not a valid worksheet name"
 NO_TASKS_FOR_TODAY = "You have no other tasks for today!"
 NO_WORKSHEET_IN_SHEET = "You have no lessons in your sheet!"
