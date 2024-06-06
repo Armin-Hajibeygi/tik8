638816686daa50ef172f142a036b3a1fa9b47af2
Armin Hajibeygi <armin.beygi2000@gmail.com>
2024-06-06 10:04:33 +0330

    feat: add sign up

--- bot.py
+++ bot.py
@@ -62,6 +62,24 @@ async def all_lessons(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def sign_up(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    response = f"Dear {update.effective_user.first_name} \n"
+    user_id = update.effective_user.id
+
+    if user_id in ids:
+        response += "You already have and account"
+    else:
+        response += f"Your code: <b><u>{user_id}</u></b> \n"
+        response += f"Please give this code to the admin to finalize your sign up ^^"
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=response,
+        reply_to_message_id=update.effective_message.id,
+        parse_mode="HTML",
+    )
+
+
 if __name__ == "__main__":
     bot = ApplicationBuilder().token(BOT_TOKEN).build()
 
@@ -69,6 +87,7 @@ if __name__ == "__main__":
     bot.add_handler(CommandHandler("start", start_command_handler))
     bot.add_handler(CommandHandler("today", today_task_command_handler))
     bot.add_handler(CommandHandler("lessons", all_lessons))
+    bot.add_handler(CommandHandler("signup", sign_up))
 
     # start bot
     bot.run_polling()
